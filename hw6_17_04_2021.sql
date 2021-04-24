64d5ce6f4ce98ac05f06b336538c290cab9a7ac6
Pavel Nedoshivin <pnedoshivin@gmail.com>
2021-04-24 14:04:40 +0300

    Урок_6_Домашнее_задание

---
+++ hw6_17_04_2021.sql
@@ -0,0 +1,69 @@
+-- Задание 1. 
+
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  target_id INT UNSIGNED NOT NULL,
+  target_type_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE target_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO target_types (name) VALUES 
+  ('messages'),
+  ('users'),
+  ('media'),
+  ('posts');
+
+
+INSERT INTO likes 
+  SELECT 
+    id, 
+    FLOOR(1 + (RAND() * 100)), 
+    FLOOR(1 + (RAND() * 100)),
+    FLOOR(1 + (RAND() * 4)),
+    CURRENT_TIMESTAMP 
+  FROM messages;
+
+
+
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  community_id INT UNSIGNED,
+  head VARCHAR(255),
+  body TEXT NOT NULL,
+  media_id INT UNSIGNED,
+  is_public BOOLEAN DEFAULT TRUE,
+  is_archived BOOLEAN DEFAULT FALSE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+-- Задание 2. Внешние ключи добавлял средствами DBeaver, поэтому без команд 
+
+-- Задание 3.
+select (select gender from profiles where user_id = likes.user_id) as gender, COUNT(*) as total from likes group by gender order by total desc limit 1;  
+   
+   
+ 
+ -- Задание 4. 
+ select COUNT(*) from likes where target_type_id = 2 and target_id in (select * from ( select user_id from profiles order by birthday desc limit 10 ) as sorted_profiles ) 
+;
+
+-- Задание 5.
+SELECT CONCAT(first_name, ' ', last_name) AS user, 
+	(SELECT COUNT(*) FROM likes WHERE likes.user_id = users.id) + 
+	(SELECT COUNT(*) FROM media WHERE media.user_id = users.id) + 
+	(SELECT COUNT(*) FROM messages WHERE messages.from_user_id = users.id) AS overall_activity 
+	  FROM users
+	  ORDER BY overall_activity
+	  LIMIT 15;
+   
